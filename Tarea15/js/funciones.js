63502669d47c30f11980abbe56ae58f3b8180e56
Jhon Bellido <jhonbellidovargas@gmail.com>
2022-11-22 08:58:03 -0500

    mejoras

--- Tarea15/js/funciones.js
+++ Tarea15/js/funciones.js
@@ -1,23 +1,29 @@
-// Ejercicio 1
+const ejercicio1 = document.getElementById("ejercicio1");
+const ejercicio2 = document.getElementById("ejercicio2");
 const res = document.getElementById("resultado");
-let importe = prompt("Importe de compra:");
-let descuento = 0;
-if (importe >= 150) {
-  descuento = importe * 0.12;
-  importe = importe - descuento;
-} else {
-  descuento = 0;
-}
-res.innerHTML = `El descuento es de ${descuento} y el importe final es de ${importe}`;
 
-// Ejercicio 2
-const res = document.getElementById("resultado");
-let nombre = prompt("Nombre del alumno:");
-let parcial = prompt("Nota del examen parcial:");
-let final = prompt("Nota del examen final:");
-let practicas = prompt("Nota del promedio de prácticas:");
+ejercicio1.onclick = function () {
+  // Ejercicio 1
+  let importe = prompt("Importe de compra:");
+  let descuento = 0;
+  if (importe >= 150) {
+    descuento = importe * 0.12;
+    importe = importe - descuento;
+  } else {
+    descuento = 0;
+  }
+  res.innerHTML = `El descuento es de ${descuento} y el importe final es de ${importe}`;
+};
 
-let promedio = (parcial * 0.25 + final * 0.5 + practicas * 0.25);
-if (promedio >= 13) {
-  res.innerHTML = `El alumno ${nombre} está aprobado con un promedio de ${promedio}`;
-}
+ejercicio2.onclick = function () {
+  // Ejercicio 2
+  let nombre = prompt("Nombre del alumno:");
+  let parcial = prompt("Nota del examen parcial:");
+  let final = prompt("Nota del examen final:");
+  let practicas = prompt("Nota del promedio de prácticas:");
+
+  let promedio = parcial * 0.25 + final * 0.5 + practicas * 0.25;
+  if (promedio >= 13) {
+    res.innerHTML = `El alumno ${nombre} está aprobado con un promedio de ${promedio}`;
+  }
+};
